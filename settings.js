624cca937f071fdf147babbd898786b61254a898
binuka2009 <binukaudayanga3@gmail.com>
2025-11-10 20:46:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "Ice~uqbVWB", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "94769089430", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
