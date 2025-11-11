8dd7bc1092b5628b54ced206b4702f91fe5cbdeb
binuka2009 <binukaudayanga3@gmail.com>
2025-11-11 12:55:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "Ice~2iXNzy", // Put Session Id Here Inside "*"
+  SESSION_ID: "Ice~sfn4lg", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "94769089430", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
